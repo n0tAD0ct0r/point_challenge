5df2983896a9159bef277fc0b61cd7047645f993
James Ross <jamesross@Jamess-MacBook-Pro.local>
2021-05-01 19:40:20 -0700

    added configs

---
+++ src/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    MONGO_URL: process.env.MONGO_URL || "mongodb://127.0.0.1:27017/twitter",
+    PORT: process.env.PORT || 4000,
+    SECRET: process.env.SECRET || "TOP_SECRET"
+}
